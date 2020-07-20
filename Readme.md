33f695b8f289419919d405b98501f890ad357cfb
성근제 <bluemen02@naver.com>
2020-07-20 22:42:04 +0900

    first commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Readme
+
+it is my study java and spring
+
+I will end this study in 1~2 month
+
+start from 0720 ~
+
+
+
+## 0720
+
+setup Spring study and Java environment
+
+and I print Hello World
+
+next day I will run this study
---
+++ jump to java/jump2java/src/jump2java/HelloWorld.java
@@ -0,0 +1,12 @@
+package jump2java;
+
+/* my first java study */
+public class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+		int a = 1;
+		String b = "Hello Java";
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
